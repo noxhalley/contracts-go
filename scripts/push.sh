f2f47e1541fa80fd8e81674deac878bf610340e0
Dang Hai <pecheule34713@gmail.com>
2025-08-03 11:43:16 +0700

    update workflow

---
+++ scripts/push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+rm -rf proto_gen && mv ./gen/go proto_gen
+
+git config --global user.name "${GIT_USER_NAME}"
+git config --global user.email "${GIT_USER_EMAIL}"
+
+git add proto_gen
+git diff --quite --exit-code || git commit -m "Update from ${VERSION}"
+
+git tag -d "${VERSION}" || true
+git tag "${VERSION}"
