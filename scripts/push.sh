3d1868f8ddbbc588002ba8f97b3de5db8b151004
Dang Hai <pecheule34713@gmail.com>
2025-08-03 15:11:56 +0700

    update script

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,9 +6,8 @@ rm -rf proto_gen && mv ./gen/go proto_gen
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 
-git checkout -b "${VERSION}" main
-
 git add proto_gen
 git diff --quiet --exit-code || git commit -m "Update from ${VERSION}"
 
-git push origin "$VERSION"
+git tag -a $VERSION -m "Version ${VERSION}"
+git push --follow-tags origin HEAD
